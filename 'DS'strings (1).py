c628ac98f0193449505345d39c463b0f5b601b3c
gopimarolix <150879937+gopimarolix@users.noreply.github.com>
2024-01-08 16:36:24 +0530

    Add files via upload

---
+++ 'DS'strings (1).py
@@ -0,0 +1,35 @@
+#positive and negative indexes#
+
+s=input('enter name:')
+print(len(s))
+i=0
+for x in s:
+    print('the char at +ve index is',i,'and char at -ve is',i-len(s),'is',x)
+    i=i+1
+
+#strip#
+
+#location=input('enter to check availability:')
+#amazon_list_locations=('vizag,vijayawada,guntur,hyderabad,bangalore')
+#if location.strip() in amazon_list_locations:
+    #print('your',location,' is available:')
+#else:
+    #print('your',location,'is available shortly.....')
+
+#reverse a string#.........
+
+#s='rangerover is a beast'
+#a=s.split()
+#for i in a[::-1]:
+    #print(i,end=' ')
+
+#complete reverse#.......
+
+#s='range rover is a beast'
+#print(s[::-1])
+
+
+
+
+
+
